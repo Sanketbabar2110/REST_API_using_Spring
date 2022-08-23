06b910238e6d23ca16fddb566095b9eff16248f7
Sanketbabar2110 <sanketbabar.sb@gmail.com>
2022-08-24 00:54:43 +0530

    second commit

--- src/main/java/com/pro/controller/MovieController.java
+++ src/main/java/com/pro/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.pro.controller;
 
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,35 +18,43 @@ import com.pro.service.MovieService;
 @RestController
 public class MovieController {
 
+	private static final Logger logger = LogManager.getLogger(MovieController.class);
+	
 	@Autowired
 	MovieService service;
-	
+
 	@PostMapping("/movies")
-	private ResponseEntity<?> add(@RequestBody Movie movie){
+	private ResponseEntity<?> add(@RequestBody Movie movie) {
 		
-		if(service.addMovie(movie))
+		logger.trace("mapping post request with body : "+movie);
+		
+		if (service.addMovie(movie)) {
+			logger.info("movie saved succssffully");
 			return ResponseEntity.ok("Movie Details saved successfully!!!");
-		else
+		}
+		else {
+			logger.info("failed to save data");
 			return ResponseEntity.ok("Failed to save Movie Details!!!");
+		}
 	}
-	
+
 	@GetMapping("/movies")
-	private ResponseEntity<?> get(){
+	private ResponseEntity<?> get() {
 		return ResponseEntity.ok(service.getMovies());
 	}
-	
+
 	@GetMapping("/movies/{Id}")
-	private ResponseEntity<?> get(@PathVariable("Id") String Id){
+	private ResponseEntity<?> get(@PathVariable("Id") String Id) {
 		return ResponseEntity.ok(service.getMovie(Integer.parseInt(Id)));
 	}
-	
+
 	@DeleteMapping("/movies/{Id}")
-	private ResponseEntity<?> delete(@PathVariable("Id") String Id){
+	private ResponseEntity<?> delete(@PathVariable("Id") String Id) {
 		return ResponseEntity.ok(service.deleteMovie(Integer.parseInt(Id)));
 	}
-	
+
 	@PutMapping("/movies/{Id}")
-	private ResponseEntity<?> update(@RequestBody Movie movie, @PathVariable("Id") String Id){
+	private ResponseEntity<?> update(@RequestBody Movie movie, @PathVariable("Id") String Id) {
 		return ResponseEntity.ok(service.updateMovie(Integer.parseInt(Id), movie));
 	}
 }
